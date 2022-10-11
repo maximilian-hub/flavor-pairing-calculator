a3c1a0cf249610dec4119ec19d313286355fde78
maximilian-hub <maximilian.spedale@gmail.com>
2022-10-10 19:00:17 -0500

    POST request now sends user input.

--- index.js
+++ index.js
@@ -4,25 +4,13 @@ const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const textParser = bodyParser.text();
-
 //server initialization:
 const app = express();              //start application
 app.listen(3000,                    //start listening for requests
   ()=> console.log("Listening at port 3000.")
 );
 app.use(express.static('public'));  //serve webpage from public directory
-
-app.post('/api', textParser, handlePostRequest);
-
-function handlePostRequest (request,response) {
-  console.log("POST request recieved.");
-  console.log(request.body);
-
-  //request body should be a list of ingredient names
-  //query database based on the request body
-  //tack query results on to the response
-}
+app.post('/api', bodyParser.json(), handlePostRequest); //route POST requests
 
 //connect to database:
 const db = new sqlite3.Database(
@@ -39,3 +27,12 @@ const db = new sqlite3.Database(
 //db.run('CREATE TABLE garlic(pairing VARCHAR(30), affinity int);');
 //db.run("INSERT INTO parsley VALUES('tomato',1), ('marjoram',1);");
 //db.run("INSERT INTO garlic VALUES('tomato',1), ('bacon',1);");
+
+function handlePostRequest (request,response) {
+  console.log("POST request recieved.");
+  console.log(request.body);
+
+  //request body should be a list of ingredient names
+  //query database based on the request body
+  //tack query results on to the response
+}
