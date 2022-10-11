9332dffb83c5fae783e74ed3ba4c3ae0326d2be5
maximilian-hub <maximilian.spedale@gmail.com>
2022-10-11 11:35:02 -0500

    Trying to get data out from db.all(), having trouble due to .all() being asynchronous.

--- index.js
+++ index.js
@@ -28,11 +28,67 @@ const db = new sqlite3.Database(
 //db.run("INSERT INTO parsley VALUES('tomato',1), ('marjoram',1);");
 //db.run("INSERT INTO garlic VALUES('tomato',1), ('bacon',1);");
 
-function handlePostRequest (request,response) {
-  console.log("POST request recieved.");
-  console.log(request.body);
+async function handlePostRequest (request,response) {
+  console.log("POST request recieved:");
+  console.log(request.body);              //should be a list of ingredient names
 
-  //request body should be a list of ingredient names
-  //query database based on the request body
-  //tack query results on to the response
+  let commonPairings = await getCommonPairings(request.body.ingredients);
+
+  response.body = {
+    status: "success",
+    body: commonPairings
+  }
 }
+
+/*
+  Expects an array of valid ingredient names.
+  Returns an array of pairings common to those ingredients.
+*/
+async function getCommonPairings(requestedIngredients) {
+  //grab all pairings of the user-requested ingredients:
+  let allPairings = await getAllPairings(requestedIngredients);
+  console.log(allPairings);
+  //look for matches
+  //let commonPairings = findMatches(allPairings);
+
+  //return matches
+  //return commonPairings;
+}
+
+/*
+  Expects an array of valid ingredient names.
+  Returns an array in which each element contains
+  the pairings of an ingredient.
+*/
+function getAllPairings(requestedIngredients) {
+  let allPairings = [];
+
+  //query database
+  for (let i=0; i<requestedIngredients.length; i++) {
+    let sql = 'SELECT * FROM ' + requestedIngredients[i] + ";";
+
+    db.all(sql, [], (err,rows) => {
+      if (err) console.error(err.message);
+      else {
+        allPairings.push(rows);
+        console.log("Query successful. Pushed rows to allPairings:");
+        console.log(rows);
+      }
+    });
+  }
+
+  console.log("Query finished. allPairings: ");
+  console.log(allPairings);
+  return allPairings;
+}
+
+/*
+  Expects an array in which each element contains
+  the pairings of an ingredient.
+
+  Returns an array of the pairings shared by all ingredients.
+*/
+function findMatches() {
+
+}
+
