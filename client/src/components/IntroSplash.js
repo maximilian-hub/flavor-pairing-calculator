0b3a51aca229b305276fd297b78206095bb834e8
maximilian-hub <maximilian.spedale@gmail.com>
2023-04-12 15:17:44 -0500

    Styled splash page. Added button.

--- client/src/components/IntroSplash.js
+++ client/src/components/IntroSplash.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function IntroSplash(props) {
+  const [active, setActive] = useState(true);
+
   return (
     <>
-      <div id="intro-splash"></div>
+      <div id="splash-overlay" className={active ? "active" : "inactive"}>
+        <div id="splash-main">
+          Welcome, new user! Please allow me to uh, show you around, show you
+          how everything works around here. What this does, what buttons to
+          press, how to read the results, and all that.
+          <div id="splash-button" onClick={() => setActive(false)}>
+            Let me in!
+          </div>
+        </div>
+      </div>
     </>
   );
 }
