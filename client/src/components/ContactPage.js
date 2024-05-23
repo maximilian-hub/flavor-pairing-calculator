7bf0921eee096fc3c9607f211370d37362b66de8
maximilian-hub <61955124+maximilian-hub@users.noreply.github.com>
2024-05-22 20:49:41 -0500

    Removed broken contact form.

--- client/src/components/ContactPage.js
+++ client/src/components/ContactPage.js
@@ -35,77 +35,7 @@ export default function ContactPage() {
   return (
     <InfoPage className="info-page">
       <InfoSection header="Contact" active="true">
-        <div id="contact-links">
-          <a
-            href="https://www.linkedin.com/in/maximilian-spedale/"
-            target="_blank"
-          >
-            <i className="fa-brands fa-linkedin linkedin" title="LinkedIn">
-              <div className="icon-bg"></div>
-            </i>
-          </a>
-          <a href="https://github.com/maximilian-hub" target="_blank">
-            <i className="fa-brands fa-square-github github" title="GitHub">
-              <div className="icon-bg"></div>
-            </i>
-          </a>
-          <a href="https://www.twitter.com/max__fpc" target="_blank">
-            <i className="fa-brands fa-square-twitter twitter" title="Twitter">
-              <div className="icon-bg"></div>
-            </i>
-          </a>
-        </div>
-
-        <h3>Email me:</h3>
-        <div className="email-form-container">
-          <form
-            id="email-form"
-            className="email-form"
-            method="post"
-            onSubmit={handleSubmit}
-          >
-            <div>
-              <label htmlFor="email">email:</label>
-              <input
-                type="text"
-                name="email"
-                placeholder="you@whatever.com"
-                onChange={handleFormChange}
-              />
-            </div>
-
-            <div>
-              <label htmlFor="extension">purpose:</label>
-              <select name="extension" id="" onChange={handleFormChange}>
-                <option value="">- What is this regarding? -</option>
-                <option value="leads">Job Lead</option>
-                <option value="db">Database Suggestion</option>
-                <option value="bugreport">Bug Report</option>
-                <option value="featurerequest">Feature Request</option>
-                <option value="advice">Advice</option>
-                <option value="encouragement">Encouragement</option>
-                <option value="other">Other</option>
-              </select>
-            </div>
-
-            <div>
-              <label htmlFor="subject">subject:</label>
-              <input type="text" name="subject" onChange={handleFormChange} />
-            </div>
-
-            <div>
-              <label htmlFor="message">your message:</label>
-              <textarea
-                type="text"
-                id="user-message"
-                name="message"
-                onChange={handleFormChange}
-              />
-            </div>
-
-            <input type="submit" value="Submit" rows="10" />
-          </form>
-        </div>
+        max.spedale@gmail.com
       </InfoSection>
     </InfoPage>
   );
